117ce8eab458869d49eaa743843e0eb74f892783
Remie Bolte <remie@collabsoft.net>
2024-11-22 12:39:32 +0100

    fix: ensure proper path

--- src/helpers/toAbsolutePath.ts
+++ src/helpers/toAbsolutePath.ts
@@ -1,8 +1,9 @@
+import { realpathSync } from 'fs';
 import { dirname, join } from 'path';
 
 export const toAbsolutePath = (relativePath: string) => {
   const [ , filePath ] = process.argv;
-  const executableFileDir = dirname(filePath);
+  const executableFileDir = dirname(realpathSync(filePath));
   const basedir = executableFileDir.substring(0, executableFileDir.indexOf('dcdx') + 4);
   return join(basedir, relativePath.replaceAll('../', ''));
 }
